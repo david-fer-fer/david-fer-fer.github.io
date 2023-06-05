ebb926a928228de527277d3bea364902a165fb4b
david-fer-fer <davidfdez225@gmail.com>
2023-06-05 15:42:18 +0200

    Big content update for the portfolio

--- gamedev_portfolio/src/components/WorkCard/index.js
+++ gamedev_portfolio/src/components/WorkCard/index.js
@@ -1,34 +1,38 @@
 import React from "react";
 import {useTheme} from "next-themes";
+import Popup from "reactjs-popup";
+import Header from "../Header";
 
-const WorkCard = ({type, src, name, description, onClick}) => {
+const WorkCard = ({type, src, name, headline, description, url}) => {
     const {theme} = useTheme();
     return (
-        <div
-            className={`overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link transition-all ease-out duration-300 hover:scale-105 cursor-pointer
-      ${
-                /*mounted && */theme === "dark" ? "hover:bg-slate-800" : "hover:bg-tiffanyBlue-100"
-            }`}
-            onClick={onClick}
-        >
-            <div
-                className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
-            >
+        <Popup trigger={<div className={`overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link transition-all ease-out duration-300 hover:scale-105 cursor-pointer
+      ${/*mounted && */theme === "dark" ? "hover:bg-slate-800" : "hover:bg-tiffanyBlue-100"
+        }`}>
+            <div className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto">
                 <Media type={type} src={src}/>
-
-                {/*<img
-          alt={name}
-          className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
-          src={img}
-        ></img>*/}
             </div>
             <h1 className="mt-5 text-3xl font-medium">
                 {name ? name : "Project Name"}
             </h1>
-            <h2 className="text-xl opacity-50">
-                {description ? description : "Description"}
+            <h2 className="text-xl opacity-80">
+                {headline ? headline : "Description"}
             </h2>
-        </div>
+            <p className="text-lg opacity-50">
+                {description ? description : "Description"}
+            </p>
+        </div>}
+               modal
+               nested
+        >
+            {close => (
+                <>
+                    <i className="fas fa-times-circle absolute -right-3 -top-3 z-10 text-gray-50 rounded-full text-2xl cursor-pointer" onClick={close}></i>
+                    <iframe src={url} allow="autoplay" data-ruffle-polyfilled=""
+                                className={"w-full h-full mx-auto absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"}/>
+                </>
+            )}
+        </Popup>
     );
 };
 
@@ -36,16 +40,23 @@ const Media = ({type, src}) => {
     const ref = React.useRef();
     const [height, setHeight] = React.useState("0px");
     const onLoad = () => {
-        setHeight(ref.current?.contentWindow.document.body.scrollHeight + "px");
+        setHeight(ref.current?.contentWindow.scrollHeight + "px");
     };
     switch (type) {
         case "iframe":
             return <iframe ref={ref}
-                           onload='javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));' src={src} allow="autoplay" data-ruffle-polyfilled="" className={"w-full"}></iframe>;
+                           width="100%" height="100%"
+                           src={src} allow="autoplay" data-ruffle-polyfilled=""></iframe>;
         case "video":
             return <video autoPlay muted loop id="myVideo">
                 <source src={src} type="video/mp4"/>
             </video>
+        case "image":
+            return <img
+                alt={"work card image"}
+                className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+                src={src}
+            ></img>
     }
 }
 
--- gamedev_portfolio/src/pages/resume.js
+++ gamedev_portfolio/src/pages/resume.js
@@ -40,7 +40,7 @@ const Resume = () => {
                         <div
                             className={`w-full ${
                                 mount && theme.theme === "dark" ? "bg-slate-800" : "bg-gray-50"
-                            } max-w-4xl p-20 mob:p-5 desktop:p-20 rounded-lg shadow-sm`}
+                            } max-w-4xl p-20 mob:p-5 desktop:p-20 rounded-lg shadow-md`}
                         >
                             <h1 className="text-3xl font-bold">{name}</h1>
                             <h2 className="text-xl mt-5">{resume.tagline}</h2>
